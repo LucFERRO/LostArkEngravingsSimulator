8d63463ae309c849cc9b4581aab1ad7467951384
Luc FERRO <luchenri.ferro@gmail.com>
2022-06-10 17:22:38 +0200

    first commit

---
+++ src/components/Line.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Line = (props) => {
+  return (
+    <div class="line">This is the line for {props.name}</div>
+  )
+}
+
+export default Line
---
+++ src/components/Table.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Line from './Line'
+
+const Table = () => {
+  return (
+    <div id="table">
+        <Line name="Necklace" />
+        <Line name="Earring1" />
+    </div>
+  )
+}
+
+export default Table
